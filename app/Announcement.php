46a8605a5f64efc789b555826cf6f07f0c137785
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2020-05-09 02:57:10 +0300

    Adds relations to Announcement model
    Also added unit tests for relations. The primary key of this model doesn't
    increase, so the incrementing is disabled. The primay key of this this model is a
    uuid.

--- app/Announcement.php
+++ app/Announcement.php
@@ -7,6 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Announcement extends Model
 {
+    public $incrementing = false;
+
     protected $fillable = [
         'id', 'slug', 'title', 'description', 'poster_image_path', 'starts_at', 'ends_at',
         'quota', 'price', 'payment_period', 'is_featured',
@@ -18,4 +20,14 @@ class Announcement extends Model
         'ends_at' => 'datetime',
         'payment_period' => PaymentPeriod::class,
     ];
+
+    public function classRoom()
+    {
+        return $this->belongsTo('App\ClassRoom');
+    }
+
+    public function applications()
+    {
+        return $this->hasMany('App\Application');
+    }
 }
---
+++ tests/Unit/AnnouncementTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Student;
+use App\ClassRoom;
+use Tests\TestCase;
+use App\Announcement;
+use App\Application;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class AnnouncementTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    public function testItBelongsToClassRoom()
+    {
+        $classRoom = factory(ClassRoom::class)->create();
+        $announcement = factory(Announcement::class)->create([
+            'class_room_id' => $classRoom->id,
+        ]);
+
+        $this->assertTrue($announcement->classRoom()->exists());
+        $this->assertEquals($classRoom->id, $announcement->classRoom->id);
+    }
+
+    public function testItShoulHasManyApplications()
+    {
+        $classRoom = factory(ClassRoom::class)->create();
+        $announcement = factory(Announcement::class)->create([
+            'class_room_id' => $classRoom->id,
+        ]);
+
+        $student1 = factory(Student::class)->create();
+        $application1 = factory(Application::class)->create([
+            'student_id' => $student1->id,
+            'announcement_id' => $announcement->id,
+        ]);
+
+        $student2 = factory(Student::class)->create();
+        $application2 = factory(Application::class)->create([
+            'student_id' => $student2->id,
+            'announcement_id' => $announcement->id,
+        ]);
+
+        $this->assertTrue($announcement->applications()->exists());
+        $this->assertEquals($application1->id, $announcement->applications[0]->id);
+        $this->assertEquals($application2->id, $announcement->applications[1]->id);
+    }
+}
