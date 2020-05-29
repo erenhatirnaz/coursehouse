5f5df4da04a59128c229c5b4d585b55a23880837
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2020-05-30 01:13:50 +0300

    Adds localization support for password reset pages

--- resources/lang/tr/passwords.php
+++ resources/lang/tr/passwords.php
@@ -1,9 +1,13 @@
 <?php
 
 return [
-    'reset'     => 'Parolanız sıfırlandı!',
-    'sent'      => 'Parola sıfırlama bağlantınız e-posta ile gönderildi!',
+    'reset_password' => "Parola Sıfırla",
+
+    'send_link' => "Parola Sıfırlama Bağlantısı Gönder",
+
+    'reset' => 'Parolanız sıfırlandı!',
+    'sent' => 'Parola sıfırlama bağlantınız e-posta ile gönderildi!',
     'throttled' => 'Tekrar denemeden önce lütfen bekleyin.',
-    'token'     => 'Parola sıfırlama kodu geçersiz.',
-    'user'      => 'Bu e-posta adresi ile kayıtlı bir üye bulunamadı.',
+    'token' => 'Parola sıfırlama kodu geçersiz.',
+    'user' => 'Bu e-posta adresi ile kayıtlı bir üye bulunamadı.',
 ];
