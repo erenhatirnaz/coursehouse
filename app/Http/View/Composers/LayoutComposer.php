aad5c230f9dd4142542774b6d4f827a865b49824
Eren Hatırnaz <erenhatirnaz@hotmail.com.tr>
2020-08-29 02:48:20 +0300

    Search placeholder is now cached for 1 hours

--- app/Http/View/Composers/LayoutComposer.php
+++ app/Http/View/Composers/LayoutComposer.php
@@ -5,6 +5,7 @@ namespace App\Http\View\Composers;
 use Illuminate\View\View;
 use Illuminate\Support\Facades\Session;
 use App\Repositories\CourseRepositoryInterface;
+use Illuminate\Support\Facades\Cache;
 
 class LayoutComposer
 {
@@ -20,12 +21,14 @@ class LayoutComposer
 
     public function compose(View $view)
     {
-        $search_placeholder = __('app.search_placeholder');
-
-        $courses = $this->courses->all();
-        if ($courses->count() > 0) {
-            $search_placeholder = $courses->random()['name'];
-        }
+        $search_placeholder = Cache::remember('search_placeholder', now()->addHour(), function () {
+            $courses = $this->courses->all();
+            if ($courses->count() > 0) {
+                return $courses->random()->name;
+            } else {
+                return __('app.search_placeholder');
+            }
+        });
 
         $view->with('search_placeholder', $search_placeholder);
 
